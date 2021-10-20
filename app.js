57b10248b551c559226fde691a06735fa7ee3390
facuzero <facuzero_95@hotmail.com>
2021-10-20 16:17:27 -0300

    subida de carpetas public css img views y app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+//  Variables   //
+const express=require('express');
+let app=express();
+const path=require('path');
+const PORT=3030;
+
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname,"/public/views/index.html"))
+})
+app.get('Detalle del producto',(req,res)=>{
+    res.sendFile(path.join(__dirname, "/public/views/productDetail.html"))
+
+})
+app.get('Carrito de compras',(req,res)=>{
+    res.sendFile(path.join(__dirname, "/public/views/productCart.html"))
+})
+app.get('Formulario de registro',(req,res)=>{
+    res.sendFile()(path.join(__dirname,"/public/views/register.html"))
+})
+app.get('Formulario de login',(req,res)=>{
+    res.sendFile()(path.join(__dirname,"/public/views/login.html"))
+})
+app.listen(PORT, ()=> console.log(`Servidor abierto en el puerto ${PORT}
+http://localhost:${PORT}`))
