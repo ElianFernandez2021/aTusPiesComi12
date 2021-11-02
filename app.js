c1ba1c0b30bdfb65611c3dc281d794d1c1d86a10
facuzero <facuzero_95@hotmail.com>
2021-11-02 18:05:07 -0300

    cambios en el app.js

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ let app=express();
 const path=require('path');
 const PORT=3030; 
 
-app.get('/',(req,res)=> {
-    res.sendFile(path.join(__dirname,"/public/views/index.html"))
-})
-app.get('/productdetail',(req,res)=>{
-    res.sendFile(path.join(__dirname, "/public/views/productDetail.html"))
+app.use(express.static('public'));
 
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname,"/views/index.html"))
 })
-app.get('Carrito de compras',(req,res)=>{
-    res.sendFile(path.join(__dirname, "/public/views/productCart.html"))
+app.get('/productDetail',(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
 })
-app.get('Formulario de registro',(req,res)=>{
-    res.sendFile()(path.join(__dirname,"/public/views/register.html"))
+app.get('/productCart',(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/productCart.html"))
 })
-app.get('Formulario de login',(req,res)=>{
-    res.sendFile()(path.join(__dirname,"/public/views/login.html"))
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 })
 app.listen(PORT, ()=> console.log(`Servidor abierto en el puerto ${PORT}
 http://localhost:${PORT}`))
