37a4e5d776ad4672a8a9d00c7c672ad7d839f261
facuzero <facuzero_95@hotmail.com>
2021-11-03 12:39:19 -0300

    Agregue una etiqueta a al logo para volver al home cuando se clickee

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ app.use(express.static('public'));
 app.get('/',(req,res)=> {
     res.sendFile(path.join(__dirname,"/views/index.html"))
 })
-app.get('/productDetail',(req,res)=>{
+app.get('/productDetail.html',(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/productDetail.html"))
 })
-app.get('/productCart',(req,res)=>{
+app.get('/productCart.html',(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/productCart.html"))
 })
-app.get('/register',(req,res)=>{
+app.get('/register.html',(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
-app.get('/login',(req,res)=>{
+app.get('/login.html',(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/login.html"))
 })
 app.listen(PORT, ()=> console.log(`Servidor abierto en el puerto ${PORT}
