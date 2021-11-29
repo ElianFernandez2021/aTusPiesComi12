971c2f1828a05cd5584a2c5068d13656c458c91a
facuzero <facuzero_95@hotmail.com>
2021-11-29 01:21:21 -0300

    agregamos templates ejs

--- src/app.js
+++ src/app.js
@@ -6,17 +6,21 @@ const PORT=3030;
 
 /* Middlewares */
 app.use(express.static('src/public'));
-app.set("view engine", "ejs")
+app.set("view  engine", "ejs")
 app.set('views', path.join(__dirname, 'views'))
 
 /* Enrutadores */
+let indexRouter=require('./routes/index')
+let loginRouter=require('./routes/login')
+let registerRouter=require('./routes/register')
+let productDetailRouter=require('./routes/productDetail')
 let productCartRouter=require('./routes/productCart')
 
 /* Routes */
-                //home
-                //Login
-                //Register
-                //ProductDetail
-app.use('/productCart',productCartRouter)
+app.use('/',indexRouter)//home
+app.use('/login',loginRouter)//Login
+app.use('/register',registerRouter)//Register
+app.use('/productDetail',productDetailRouter)//ProductDetail
+app.use('/productCart',productCartRouter)//ProductCart
 app.listen(PORT, ()=> console.log(`Servidor abierto en el puerto ${PORT}
 http://localhost:${PORT}`))
