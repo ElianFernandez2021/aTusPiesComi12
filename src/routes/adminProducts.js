18725149901fd21261f4bffd2fa7448201e66389
facuzero <facuzero_95@hotmail.com>
2021-12-16 21:01:55 -0300

    agrego estilos a productos

--- src/routes/adminProducts.js
+++ src/routes/adminProducts.js
@@ -4,16 +4,16 @@ let controller=require('../controllers/adminController')
 let upload = require('../middlewares/uploadProductFile')
 
 /* Get - Product */
-router.get('/product',controller.adminCategory)
-router.get('/product/category/:id',controller.adminSelectionCategory)
+router.get('/products',controller.adminCategory)
+router.get('/products/category/:id',controller.adminSelectionCategory)
 /* product create */
-router.get('/product/create',controller.create) //Renderisa la vista de carga de producto
-router.post('/product', upload.single('image'), controller.store)//Guarda la informacion que almacena en la base de datos(json)
+router.get('/products/create',controller.create) //Renderisa la vista de carga de producto
+router.post('/products', upload.single('image'), controller.store)//Guarda la informacion que almacena en la base de datos(json)
 /* product edit */
-router.get('/product/edit/:id?',controller.edit)//Renderisa la vista de edicion de producto y recibe un parametro obligatorio que seria el id
-router.put('/product/:id',upload.single('image'),controller.update)//Guarda la informacion de un parametro obligatorio y actualiza la base de datos
+router.get('/products/edit/:id?',controller.edit)//Renderisa la vista de edicion de producto y recibe un parametro obligatorio que seria el id
+router.put('/products/:id',upload.single('image'),controller.update)//Guarda la informacion de un parametro obligatorio y actualiza la base de datos
 
 /* product remove */
-router.delete('/product/:id',controller.fatality)
+router.delete('/products/:id',controller.fatality)
 
 module.exports = router
