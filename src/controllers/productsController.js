3c358360f45f474a4a07d0a2b932374378f69945
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-03-14 20:06:56 -0300

    agrega color y marca a vista  productDetail

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -39,18 +39,22 @@ let controller={
             where: {
                 id: req.params.id,
             },
-            include: [{ association: 'images' }]
+            include: [{association:'colors'},
+            {association:'images'},{association:'marca'},]
         })
             .then(((product) => {
                 Products.findAll({
-                    include: [{ association: 'images' }],
+                    include: [{association:'colors'},
+                    {association:'images'},{association:'marca'},],
                     where: {
                         id: req.params.id,
                     }
                 })
-                    .then((relatedProducts) => {
+                    .then((relatedProducts,colors,marca) => {
                         res.render("productDetail", {
                             product,
+                            colors,
+                            marca,
                             sliderTitle: "Productos relacionados",
                             sliderProducts: relatedProducts,
                             session: req.session
