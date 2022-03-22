a233bd2792558f2b3d4c28c5ab60c0621484687f
facuzero <facuzero_95@hotmail.com>
2022-03-22 15:12:11 -0300

    commit

--- src/routes/api/apiCart.js
+++ src/routes/api/apiCart.js
@@ -3,6 +3,10 @@ let router = express.Router();
 
 let {show,add,remove,removeItem,empty} = require("../../controllers/cartController")
 
-router.get("/api/item/:id", add);
+router.get("/api/item/:id", show);
+router.post("/api/item/:id",add);
+router.post("/api/item/:id",remove);
+router.post("/api/item/:id",removeItem);
+router.post("/api/item/:id",empty);
 
 module.exports= router;
